c9f75d2e4f34c2da755a6a2acc547777ad8310ec
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2019-06-13 13:01:39 +0300

    Rename alter komutu.sql to BasitSqlKomutlari.sql

---
+++ BasitSqlKomutlari.sql
@@ -0,0 +1,5 @@
+/*use okul -----> Kullanılacak veritabanı seçilir.  */
+/*alter table ogr alter column notlar smallint ---> notlar kolonunun tipini değiştirir.*/
+/*Drop table ogr2---> tabloyu siler. */
+/*alter table ogr drop column BaslamaTarih ---> baslamaTarihi simli kolonu siler. */
+/*truncate table ogr ---> belirtilen tablodaki verileri siler. */
--- alter komutu.sql
+++
@@ -1,5 +0,0 @@
-/*use okul -----> Kullanýlacak veritabaný seçilir.  */
-/*alter table ogr alter column notlar smallint ---> notlar kolonunun tipini deðiþtirir.*/
-/*Drop table ogr2---> tabloyu siler. */
-/*alter table ogr drop column BaslamaTarih ---> baslamaTarihi simli kolonu siler. */
-/*truncate table ogr ---> belirtilen tablodaki verileri siler. */
